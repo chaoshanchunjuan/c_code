b206997238d2c2beb2c77e43df155fc74512d7a3
超山春卷 <2998772276@qq.com>
2022-12-22 22:48:37 +0800

    test_12_22

---
+++ test_12_22/test_12_22.cpp
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <math.h>
+#pragma warning (disable:4996)
+//
+//int main()
+//{
+//
+	//打印菱形
+	//int n = 0;
+	//scanf("%d", &n);
+	//int k = n-1;
+	//for (int i = 0; i < n; i++)
+	//{
+	//	for (int j = 0; j<k; j++)
+	//	{
+	//		printf(" ");
+	//	}
+	//	k--;
+	//	for (int k = 0; k < i * 2 + 1; k++)
+	//	{
+	//		printf("*");
+	//	}
+	//	printf("\n");
+	//}
+	//int l = 1;
+	//for (int i = 0; i < n - 1; i++)
+	//{
+	//	for (int j = 0; j <l ; j++)
+	//	{
+	//		printf(" ");
+	//	}
+	//	l++;
+	//	for (int k = 0; k <n*2-3-i*2 ; k++)
+	//	{
+	//		printf("*");
+	//	}
+	//	printf("\n");
+	//}
+	//
+//
+	//return 0;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int k = 1;
+//	int k2 = n * 2 - 1;
+//	int l = 0;
+//	for (int i = 0; i < 2 * n - 1; i++)
+//	{
+//		//打印空格
+//		if (i <= n)
+//		{
+//			for (int j = 0; j < n - 1 - i; j++)
+//			{
+//				printf(" ");
+//			}
+//		}
+//		else
+//		{
+//			for (int j = 0; j < k; j++)
+//			{
+//				printf(" ");
+//			}
+//			k++;
+//		}
+//		//打印*
+//		if (i<=n)
+//		{
+//			for (int i = 0; i < l * 2 + 1; i++)
+//			{
+//				printf("*");
+//			}
+//		}
+//		else
+//		{
+//			for (int j = 0; j < k2 - 2*l; j++)
+//			{
+//				printf("*");
+//			}
+//			l++;
+//		}
+//		printf("\n");
+//	}
+//
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	for (int i = 100; i < 1000; i++)
+//	{
+//		int sum = pow(i % 10, 3) + pow(i / 10 % 10, 3) + pow(i / 100 % 10, 3);
+//		if (i == sum)
+//		{
+//			printf("%d\n", i);
+//		}
+//	}
+//
+//	return 0;
+//}
+//
+int main()
+{
+
+
+
+	return 0;
+}
