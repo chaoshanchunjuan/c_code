d3b341799b4c43bc1b77eb7c08d042fff1aca6b9
超山春卷 <2998772276@qq.com>
2022-12-27 22:10:56 +0800

    c_12_27

---
+++ c_12_27/c_12_27/c_12_27.c
@@ -0,0 +1,46 @@
+//#include <stdio.h>
+//#pragma warning (disable:4996)
+//
+//void start()
+//{
+//	printf("*****************************\n");
+//	printf("********  1. play   *********\n");
+//	printf("********  2. exit   *********\n");
+//	printf("*****************************\n");
+//}
+//
+//void play()
+//{} 
+//
+//int main()
+//{
+//	start();
+//	int i = 0;
+//	scanf("%d", &i);
+//	if (i == 1)
+//	{
+//		play();
+//	}
+//	else
+//	{
+//		printf("ÍË³ö\n");
+//	}
+//	return 0;
+//}
+//int numberOfSteps(int num) {
+//    int count = 0;
+//    while (num)
+//    {
+//        if (num % 2 == 0)
+//        {
+//            num /= 2;
+//            count++;
+//        }
+//        else
+//        {
+//            num -= 1;
+//            count++;
+//        }
+//    }
+//    return count;
+//}
