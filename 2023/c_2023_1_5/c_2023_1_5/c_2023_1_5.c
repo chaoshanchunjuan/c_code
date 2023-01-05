d8d5bc7a6d4d4cc47c42e7ce5ff48367b8323e45
超山春卷 <2998772276@qq.com>
2023-01-05 22:38:51 +0800

    c_2023_1_5

---
+++ 2023/c_2023_1_5/c_2023_1_5/c_2023_1_5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+//int main()
+//{
+	//ÕûÐÍÌáÉý
+	//char i = 3;
+	//00000000000000000000000000000011
+	//char j = 5;
+	//00000000000000000000000000000101
+	//printf("%d", i + j);
+	//00000000000000000000000000001000
+	//00001000
+	//00000000000000000000000000001000
+	//8
+	//return 0;
+//}
+
+int main()
+{
+	//int a = 0;
+	//short i = 0;
+	//printf("%d\n", sizeof(int));
+	//printf("%d\n", sizeof(short));
+	//printf("%d\n", sizeof(long));
+
+	return 0;
+}
---
+++ 2023/easyx_1_5/easyx_1_5/easyx.cpp
@@ -0,0 +1,32 @@
+//#include <easyx.h>
+//#include <stdio.h>
+//
+//int main()
+//{
+//	//initgraph(100, 100);
+//	//setaspectratio(1, -1);
+//	//setorigin(50, 50);
+//	////circle(0, 0, 20);
+//
+//	//int x, y;
+//	//for (int i = 0; i < 300; i++)
+//	//{
+//	//	x = rand() % (100 + 1) - 50;
+//	//	y = rand() % (100 + 1) - 50;
+//	//	putpixel(x, y, GREEN);
+//	//	putpixel(x, y, RED);
+//	//	putpixel(x, y, YELLOW);
+//	//}
+//
+//	initgraph(800, 600);
+//	setaspectratio(1, -1);
+//	setorigin(400, 300);
+//	//line(0, 0, 4, 4);
+//	//line(4, 0, 0, 4);
+//
+//
+//	getchar();
+//	closegraph();
+//	return 0;
+//}
+
