22e1bbdb06719dda4bb6ccf4b401e4e01257925f
超山春卷 <2998772276@qq.com>
2023-02-17 22:01:45 +0800

    2023_2_17

---
+++ 2023/2023_2_17/2023_2_17/2023_2_17 .c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+#pragma warning(disable:4996)
+//
+//int main()
+//{
+//	//秒表
+//	
+//	for (int shi = 11; shi >= 0; shi--)
+//	{
+//		for (int fen = 59; fen >= 0; fen--)
+//		{
+//			for (int miao = 59; miao >= 0; miao--)
+//			{
+//				printf("%d:%d:%d\n", shi, fen, miao);
+//				Sleep(10);
+//			}
+//		}
+//	}
+//	return 0;
+//}
+//
+//int count(int arr)
+//{
+//	int count1 = 0;//zong
+//	int count2 = 0;//hege
+//	int count3 = 0;//buhege
+//	for (int i = 0;arr != '\0'; i++)
+//	{
+//		if (arr ==1)
+//		{
+//			count1++;
+//			count2++;
+//		}
+//		else if (arr == 0)
+//		{
+//			count3++;
+//		}
+//		arr++;
+//	}
+//	return  count2/count1  ;
+//}
+//int main()
+//{
+//	char arr[10] = "0";
+//	scanf("%s", arr);
+//	//int ret=count(arr);
+//	//printf("合格率:%d\n", ret);
+//	double count1 = 0;//zong
+//		double count2 = 0;//hege
+//		double count3 = 0;//buhege
+//		for (int i = 0;arr[i] != '\0'; i++)
+//		{
+//			if (arr[i] == '1')
+//			{
+//				count1++;
+//				count2++;
+//			}
+//			else if (arr[i] == '0')
+//			{
+//				count3++;
+//				count1++;
+//			}
+//		}
+//		printf("%.2lf\n", count2 / count1);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	char arr[10] = "0";
+//	scanf("%s", arr);
+//	
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
